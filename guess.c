3d1f6e07d7c7a27ef3b2bdbf03e005b56421187c
Jessica Sewe <jessicasewe89@gmail.com>
2023-11-12 11:50:27 +0000

    guess game working

--- guess.c
+++ guess.c
@@ -16,7 +16,30 @@ int main(){
 
     //generate a random number between MIN and Max
     answer = (rand() % MAX) + MIN;
-    printf("%d\n", answer);
+    
+    do
+    {
+        printf("Enter a guess: ");
+        scanf("%d", &guess);
+        if(guess < answer)
+        {
+            printf("Too low\n");
+        }
+        else if(guess > answer)
+        {
+            printf("Too high\n");
+        }
+        else{
+            printf("you are correct!\n");
+        }
+        guesses++;
+    } while (guess != answer);
+
+    printf("**************************\n");
+    printf("Answer: %d\n", answer);
+    printf("Guesses: %d\n", guesses);
+    printf("**************************\n");
+    
 
 
 
